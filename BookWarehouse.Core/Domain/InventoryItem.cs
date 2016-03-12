3a55570670cabeba02505c818806139654a0f328
whoiskevinrich <whoiskevinrich@gmail.com>
2016-03-11 22:49:04 -0800

    Minor Changes to DBSets for Normalcy

---
+++ BookWarehouse.Core/Domain/InventoryItem.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace BookWarehouse.Core.Domain
+{
+    public class InventoryItem
+    {
+        [Key]
+        public Guid InventoryItemId { get; set; }
+
+        public string Edition { get; set; }
+
+        public int Quality { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Guid TitleId { get; set; }
+
+        [ForeignKey("TitleId")]
+        public virtual Title Title { get; set; }
+
+        public Guid WarehouseId { get; set; }
+        
+        [ForeignKey("WarehouseId")]
+        public virtual Warehouse Warehouse { get; set; }
+    }
+}
--- BookWarehouse.Core/Domain/Title.cs
+++ BookWarehouse.Core/Domain/Title.cs
@@ -1,6 +1,5 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookWarehouse.Core.Domain
 {
@@ -12,18 +11,7 @@ namespace BookWarehouse.Core.Domain
         public string Isbn { get; set; }
 
         public int YearPublished { get; set; }
-
-        public decimal Price { get; set; }
-
+        
         public int QuantityOnHand { get; set; }
-
-        public string Edition { get; set; }
-
-        public int Quality { get; set; }
-
-        public Guid WarehouseId { get; set; }
-
-        [ForeignKey("WarehouseId")]
-        public virtual Warehouse Warehouse { get; set; }
     }
 }
