21a61211e6927bc53b57a6054a95e3a0ce3bdd49
whoiskevinrich <whoiskevinrich@gmail.com>
2016-03-12 00:11:05 -0800

    Updated DbContext Properties

--- BookWarehouse.Core/Data/WarehouseContext.cs
+++ BookWarehouse.Core/Data/WarehouseContext.cs
@@ -1,9 +1,12 @@
 ﻿using System.Data.Entity;
+using BookWarehouse.Core.Domain;
 
 namespace BookWarehouse.Core.Data
 {
     public class WarehouseContext : DbContext
     {
-
+        public DbSet<InventoryItem> InventoryItems { get; set; }
+        public DbSet<Title> Titles { get; set; }
+        public DbSet<Warehouse> Warehouses { get; set; }
     }
 }
