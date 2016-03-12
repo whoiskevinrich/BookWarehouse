fee096afacbede184a7d7515dfcf4518bbd4ebf9
whoiskevinrich <whoiskevinrich@gmail.com>
2016-03-12 00:13:49 -0800

    Updated Autofac Configuration: it's always helpful to actually *register* autofac.

--- BookWarehouse.Web/Global.asax.cs
+++ BookWarehouse.Web/Global.asax.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using BookWarehouse.Core.Data;
 
 namespace BookWarehouse.Web
 {
@@ -17,6 +15,15 @@ namespace BookWarehouse.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AutofacConfig.RegisterDependencies();
+
+            //There is a one-time performance hit when initializing the model,
+            // I like taking it right after publish
+            using (var context = new WarehouseContext())
+            {
+                context.Database.Initialize(false);
+            }
         }
     }
 }
