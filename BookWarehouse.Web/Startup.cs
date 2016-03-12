96bfe89a686f0f58c3c89bc235ff3d834c88e411
whoiskevinrich <whoiskevinrich@gmail.com>
2016-03-11 21:08:40 -0800

    Initial Commit

---
+++ BookWarehouse.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookWarehouse.Web.Startup))]
+namespace BookWarehouse.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
